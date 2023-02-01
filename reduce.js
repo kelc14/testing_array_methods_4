52dc9e31f11e70daf2aaea250a1d6b70ee6e2339
kelsey <kc.breen3@gmail.com>
2023-02-01 16:21:59 -0500

    completed exercises

--- reduce.js
+++ reduce.js
@@ -6,7 +6,13 @@ Examples:
     extractValue(arr,'name') // ['Elie', 'Tim', 'Matt', 'Colt']
 */
 
-function extractValue(arr, key) {}
+function extractValue(arr, key) {
+        return arr.reduce(function(acc,nextValue){
+        //put this above return so that it includes the first value
+         acc.push(nextValue[key]);
+         return acc;
+    }, []);
+};
 
 /*
 Write a function called vowelCount which accepts a string and returns an object with the keys as the vowel and the values as the number of times the vowel appears in the string. This function should be case insensitive so a lowercase letter and uppercase letter should count
@@ -19,7 +25,33 @@ Examples:
     vowelCount('I Am awesome and so are you') // {i: 1, a: 4, e: 3, o: 3, u: 1};
 */
 
-function vowelCount(str) {}
+// const str = 'lollapalooze';
+// let charFreq = {};
+// for (let char of str){
+//     if(charFreq[char]){
+//         charFreq[char] +=1;
+//     }
+//     else {
+//         charFreq[char] = 1;
+//     }
+// }
+
+
+function vowelCount(str) {
+    letterArray = Array.from(str);
+    let vowelStr = 'aeiou';
+    return letterArray.reduce(function(acc,next){
+        let lowerCased= next.toLowerCase();
+        if(vowelStr.indexOf(lowerCased) !== -1){
+            if(acc[lowerCased]){
+                acc[lowerCased]++;
+            } else{
+                acc[lowerCased] = 1;
+            }
+        }
+        return acc;
+    }, {})
+    }
 
 /*
 Write a function called addKeyAndValue which accepts an array of objects and returns the array of objects passed to it with each object now including the key and value passed to the function.
@@ -36,7 +68,19 @@ Examples:
        ]
 */
 
-function addKeyAndValue(arr, key, value) {}
+// function addKeyAndValue(arr, key, value) {
+//     arr.forEach(function(obj){
+//         return obj[key] = `${value}`;
+//     })
+//     return arr;
+// };
+
+function addKeyAndValue(arr, key, value) {
+    return arr.reduce(function(acc,next,idx){
+        acc[idx][key] = value;
+        return acc;
+    }, arr)
+};
 
 /*
 Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray. 
@@ -59,5 +103,31 @@ Examples:
     
     partition(names, isLongerThanThreeCharacters) // [['Elie', 'Colt', 'Matt'], ['Tim']]
 */
+// let minScore = midtermScores.reduce(function(min,nextScore){
+//     //     if(nextScore < min){
+//     //         return nextScore
+//     //     };
+//     //     return min;
+//     // })
 
-function partition(arr, callback) {}
+function partition(arr, callback) {
+    let trueArray = [];
+    let falseArray = [];
+    //for some reason i can't get the first value of the array to work with reduce
+    if (callback(arr[0])){
+        trueArray.push(arr[0]);
+    }
+    else {
+        falseArray.push(arr[0]);
+    };
+    ///so i had to call it separately. this does every other value
+    arr.reduce(function(acc, next){
+        if (callback(next)){
+            return trueArray.push(next);
+        }
+        else {
+            return falseArray.push(next);
+        }
+    })
+    return [trueArray, falseArray];
+};
